7ff98c1f0ee67206a01f18b9558a54f6d7cce01c
Lahiru D K Yapa <lahirudinelka0611@gmail.com>
2023-08-06 15:13:35 +0530

    first commit

---
+++ database/migrations/2023_07_20_162331_rename_column_in_employees_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('employees', function (Blueprint $table) {
+            $table->renameColumn('name', 'full_name');
+            $table->renameColumn('contact_no', 'Mobile_no');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('employees', function (Blueprint $table) {
+            $table->renameColumn('full_name', 'name');
+            $table->renameColumn('Mobile_no', 'contact_no');
+        });
+    }
+};
